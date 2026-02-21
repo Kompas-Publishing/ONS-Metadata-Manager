6174a4ea2d1fcd78d8c313f0ccf27b79faf17da5
nuallan-lampe <nuallan.lampe@kompaspublishing.nl>
2026-02-21 16:24:58 +0100

    Forgot an import

--- client/src/components/app-sidebar.tsx
+++ client/src/components/app-sidebar.tsx
@@ -25,6 +25,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useToast } from "@/hooks/use-toast";
+import { cn } from "@/lib/utils";
 import {
   LayoutDashboard,
   FilePlus,
